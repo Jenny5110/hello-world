fe805fec61092e63e6e23b50539f41efecac7e0b
Thanos Pakoulieris <thanospak@gmail.com>
2017-11-06 21:10:08 +0200

    add checkText.java

---
+++ checkText.java
@@ -0,0 +1,53 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class checkText{
+	/* Total words */
+	static int words = 0;
+
+		/**
+		 * Splits the words in the text, pushes them into a List and counts them.
+		 */
+		public List<String> wordSplit(String text){
+
+		StringBuilder sb = new StringBuilder(text);
+		/* Avoiding errors */
+		int endOfLine = text.length() - 1;
+
+		/* Changes its value to true when a whitespace is followed by a character */
+		boolean word = false;
+
+		/* Loops through the text and counts the words */
+		for(int i=0; i < text.length() ; i++ ) {
+
+			 boolean whiteSpace = Character.isWhitespace(text.charAt(i));
+			 boolean character = !Character.isWhitespace(text.charAt(i));
+
+			if (character && i == 0) {
+				words++;
+			}else if (whiteSpace && i == 0){
+				sb.deleteCharAt(i);
+			}else if (whiteSpace && i == endOfLine){
+
+				break;
+			}else if (whiteSpace && !Character.isWhitespace(text.charAt(i + 1))){
+
+				word = true;
+
+			} else if (word == true) {
+				words++;
+				word = false;
+			}
+
+		}
+		/* Replacing two or more whitspaces with one whitespace */
+		String after = text.trim().replaceAll(" +", " ");
+
+		/* Creating the list 'words' and fiils it with every word on our text */
+		List<String> wordsList = Arrays.asList(after.split("\\s*,\\s*"));
+
+		return wordsList;
+	}
+}
