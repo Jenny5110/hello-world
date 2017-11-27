e6f68b20e402eedbed3da70789439bcbe40a01c5
panagiot13 <32767507+panagiot13@users.noreply.github.com>
2017-11-27 23:12:28 +0200

    Update Leksi.java
    Έβαλα κάτι setes και getes τα κεφαλαία τα κάνει μικρά και διαμορφώνει μια "ωμή λέξη"

--- Leksi.java
+++ Leksi.java
@@ -5,6 +5,8 @@ public class Leksi {
 	private int tonos;/*Βρίσκει τη θέση του τόνου*/
 	private int dialitika;/*Βρίσκει τη θέση των διαλυτικών*/
 	private int dialtonos;/*Βρίσκει τη θέση των διαλυτικών με τόνο*/
+        private StringBuffer raw;/*Δημιουργεί τη λέξη χωρις τόνους ή διαλυτικά με μικρά*/
+	private StringBuilder lower;/*ότι κεφαλάιο βρει στη λέξη το κάνει μικρό*/
 
 	public Leksi(String lex) {
 		this.fonien = 0;
@@ -14,6 +16,15 @@ public class Leksi {
 		this.dialtonos = -1;
 		int count;
 		String a;
+		this.lower = new StringBuilder(lex);
+		for (int index = 0; index < lower.length(); index++) {
+		    char c = lower.charAt(index);
+		    if (Character.isUpperCase(c)) {
+		        lower.setCharAt(index, Character.toLowerCase(c));
+		    }
+		}
+		lex = lower.toString();
+		this.raw = new StringBuffer(lex);
 		for (int i=0;i < lex.length(); i++){
 			count = i;
 			a = lex.substring(count, count+1);
@@ -25,15 +36,98 @@ public class Leksi {
 					a.equals("ώ") || a.equals("Ά") || a.equals("Έ") || a.equals("Ή") || a.equals("Ί") || a.equals("Ύ") || 
 					a.equals("Ό") || a.equals("Ώ")){
 				this.tonos = i;
+				if (a.equals("ά")){
+					this.raw.replace(i, i+1, "α");
+				} else if (a.equals("έ")){
+					this.raw.replace(i, i+1, "ε");
+				} else if (a.equals("ή")){
+					this.raw.replace(i, i+1, "η");
+				} else if (a.equals("ί")){
+					this.raw.replace(i, i+1, "ι");
+				} else if (a.equals("ύ")){
+					this.raw.replace(i, i+1, "υ");
+				} else if (a.equals("ό")){
+					this.raw.replace(i, i+1, "ο");
+				} else {
+					this.raw.replace(i, i+1, "ω");
+				}
 			} else if (a.equals("ϊ") || a.equals("ϋ")) {
 				this.dialitika = i;
+				if (a.equals("ϊ")){
+					this.raw.replace(i,  i+1,  "ι");
+				} else {
+					this.raw.replace(i,  i+1, "υ");
+				}
 			} else if (a.equals("ΐ") || a.equals("ΰ")) {
 				this.dialtonos = i;
+				if (a.equals("ΐ")){
+					this.raw.replace(i,  i+1,  "ι");
+				} else {
+					this.raw.replace(i,  i+1, "υ");
+				}
 			} else {
 				this.simfono ++;
 			}
 			
 		}
+	}
+	
+	public void setFonien(int fonien) {
+		this.fonien = fonien;
+	}
+	
+	public int getFonien() {
+		return this.fonien;
+	}
+	
+	
+	
+	public void setSimfono(int simfono) {
+		this.simfono = simfono;
+	}
+	
+	public int getSimfono() {
+		return this.simfono;
+	}
+	
+	
+	
+	public void setTonos(int tonos) {
+		this.tonos = tonos;
+	}
+	
+	public int getTonos() {
+		return this.tonos;
+	}
+	
+	
+	
+	public void setDialitika(int dialitika) {
+		this.dialitika = dialitika;
+	}
+	
+	public int getDialitika() {
+		return this.dialitika;
+	}
+	
+	
+	
+	public void setDialtonos(int dialtonos) {
+		this.dialtonos = dialtonos;
+	}
+	
+	public int getDialtonos() {
+		return this.dialtonos;
+	}
+	
+	
+	
+	public void setRaw(StringBuffer raw) {
+		this.raw = raw;
+	}
+	
+	public StringBuffer getRaw() {
+		return this.raw;
 	}
 	
 	public String toString(){
