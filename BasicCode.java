1cb53553e88a54d4fbc01e830bf30a8e04ea458a
panagiot13 <32767507+panagiot13@users.noreply.github.com>
2018-01-07 15:14:20 +0200

    Update BasicCode.java

--- BasicCode.java
+++ BasicCode.java
@@ -1,9 +1,12 @@
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.TreeMap;
-import java.util.Scanner;
+/**
+ * Contains our main class
+ * 
+ * @author Panagiotis Skias, Dimitra Balabani, Ioannis Florios
+ *
+ */
 
 /*Δέχεται ένα κείμενο και ελέγχει αν κάθε λέξη περιέχεται στο λεξικό. Αν περιέχεται στο λεξικό είναι σωστή αν όχι είναι λάθος*/
 public class BasicCode {
@@ -15,12 +18,10 @@ public class BasicCode {
 		TreeMap<String,String> Dictionary = DictionaryBuild.readRecord();
 		List<String> wordsList = checkText.wordSplit(text);
 		String word;
-		StringBuilder upper;
 		String persynt; /*Συντομογραφία*/
 		String kefprot; /*Κεφαλαίο το πρώτο γράμμα*/
 		String lex = null; /*Η λέξη που εξάγεται κάθε φορά από την λίστα*/
-		char c;
-		ArrayList <String> endexomenes = new ArrayList();
+		ArrayList <String> endexomenes = new ArrayList<String>();
 		/*Ως key λέξη raw και ως value η λέξη με τόνους κλπ*/
 		for (int i = 0; i < wordsList.size(); i++){
 			word = wordsList.get(i);
@@ -30,34 +31,36 @@ public class BasicCode {
 			String wordb = lower.toString();
 			if (Dictionary.containsValue(wordb) == false && Dictionary.containsValue(word) == false ) {
 				
-				System.out.printf("Στη λέξη %s εντοπίστηκε κάποιο λάθος \n", word);
+				System.out.printf("\t**Στη λέξη %s εντοπίστηκε κάποιο λάθος**\n", word);
+				System.out.println();
 				/*Περιπτωση όλων κεφαλαίων*/
 				persynt = Kefalaio(word, word.length());
 				
-				/*Περίπτωση κεφαλαίο πρώτο γράμμα*/
+			    /*Περίπτωση κεφαλαίο πρώτο γράμμα*/
 				kefprot = Kefalaio(word, 0);
 				
 				if (Dictionary.containsValue(persynt)) {
-					System.out.printf("Στη λέξη %s έπρεπε όλα τα γράμματα να είναι κεφαλαία \n", word);
-					System.out.printf("Άρα η λέξη γίνεται %s \n",persynt);
+					System.out.printf("->Στη λέξη %s έπρεπε όλα τα γράμματα να είναι κεφαλαία \n", word);
+					System.out.printf("->Άρα η λέξη γίνεται %s \n",persynt);
 				} else if (Dictionary.containsValue(kefprot)) {
-					System.out.printf("Στη λέξη %s έπρεπε τo πρώτο γράμμα να είναι κεφαλαία \n", word);
-					System.out.printf("Άρα η λέξη γίνεται %s \n", kefprot);
+					System.out.printf("->Στη λέξη %s έπρεπε τo πρώτο γράμμα να είναι κεφαλαία \n", word);
+					System.out.printf("->Άρα η λέξη γίνεται %s \n", kefprot);
 				} else {
 					endexomenes = lathosstofonien.lathosSearch(word);
-					ArrayList <String> lyseis = new ArrayList();
+					ArrayList <String> lyseis = new ArrayList <String>();
 					for (int k=0; k < endexomenes.size(); k++) {
 						lex = endexomenes.get(k);
 						if (Dictionary.containsKey(lex)) {
 							lyseis.add(Dictionary.get(lex));
 						}
 					}
-					System.out.println("Οι προτεινόμενες λύσεις είναι :");
-				    for (int a = 0; a < lyseis.size(); a++) {
-				    	System.out.println(lyseis.get(a));
-				    }
+					System.out.printf("Οι προτεινόμενες λύσεις για τη λέξη %s είναι :\n",word );
+					for (int a = 0; a < lyseis.size(); a++) {
+						System.out.printf("\t\t-> %s\n",lyseis.get(a));
+					}
 				}
 			}
+			System.out.println();
 		}
 	}
 	
